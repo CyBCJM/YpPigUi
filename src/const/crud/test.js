1b7f2f7fff640671ebd6a6c1ad2dc3bb9cec55a7
cy3329520 <963817195@qq.com>
2020-03-26 11:13:22 +0800

    创建

---
+++ src/const/crud/test.js
@@ -0,0 +1,20 @@
+export const tableOption = {
+  "border": true,
+  "index": true,
+  "indexLabel": "序号",
+  "stripe": true,
+  "menuAlign": "center",
+  "align": "center",
+  "searchMenuSpan": 6,
+  "column": [
+	  // {
+    //   "type": "input",
+    //   "label": "id",
+    //   "prop": "id"
+    // },
+    {
+      "type": "input",
+      "label": "名称",
+      "prop": "name"
+    }  ]
+}
